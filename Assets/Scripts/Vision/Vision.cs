38884c5179be61300ea08124bfa6e223831f1e82
Unknown <david.lafreniere@live.ca>
2018-01-17 13:50:12 -0500

    Animations, flip semi-fix

--- Assets/Scripts/Vision/Vision.cs
+++ Assets/Scripts/Vision/Vision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Vision : MonoBehaviour
 {
-	public float maxAngle = 50;
+	public float maxAngle = 360;
 
 	void Update()
 	{
@@ -12,15 +12,15 @@ public class Vision : MonoBehaviour
 		Vector3 dir = Input.mousePosition - pos;
 		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
-		if (Mathf.Abs(angle) <= maxAngle)
-		{
+		/*if (Mathf.Abs(angle) <= maxAngle)
+		{*/
 			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-		}
+		/*}
 		// Snap to maxAngle° if angle greater than that
 		else
 		{
 			transform.rotation = Quaternion.AngleAxis(maxAngle * ((angle > 0) ? 1 : -1), Vector3.forward);
-		}
+		}*/
 	}
 
 	void OnTriggerEnter2D(Collider2D coll)
