f77bd3aa5a78e1ed74990442b960723070b0cc8d
Unknown <david.lafreniere@live.ca>
2018-01-17 05:23:34 -0500

    Slowdown on bucket

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class MovingObject : MonoBehaviour {
 
     public float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+    [System.NonSerialized] public bool speedChanged;
     protected Rigidbody2D m_Rigidbody2D;
 
     // Use this for initialization
     void Start () {
-		
+        speedChanged = false;
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -19,9 +21,13 @@ public class MovingObject : MonoBehaviour {
 
     public IEnumerator ChangeSpeedFor(float newMaxSpeed, float time)
     {
+        Debug.Log("Speed change start");
         float oldSpeed = m_MaxSpeed;
+        speedChanged = true;
         m_MaxSpeed = newMaxSpeed;
         yield return new WaitForSeconds(time);
         m_MaxSpeed = oldSpeed;
+        speedChanged = false;
+        Debug.Log("Speed returned");
     }
 }
